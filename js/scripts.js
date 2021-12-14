43973a72433649706d6e12c8f52e93f4905a1052
Mikul Muzumdar <74882151+mikul109@users.noreply.github.com>
2021-12-14 15:48:11 -0500

    update

--- js/scripts.js
+++ js/scripts.js
@@ -84,7 +84,7 @@ function fadeIn(el, display) {
 
 
 function scrollbarFunction() {
-  // progress bar for scroll
+  // progress bar & circle for scroll
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
